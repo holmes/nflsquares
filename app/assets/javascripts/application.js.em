363686f5781c45723e33f18ebb155b45ed9d8880
Jason Holmes <holmes@squareup.com>
2014-01-19 10:10:37 -0800

    Initial Commit

---
+++ app/assets/javascripts/application.js.em
@@ -0,0 +1,8 @@
+#= require handlebars
+#= require ember
+#= require ember-data
+#= require_self
+#= require nflsquares
+
+# for more details see: http://emberjs.com/guides/application/
+window.Nflsquares = Ember.Application.create()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nflsquares::Application.config.secret_key_base = 'c6f93a9bb9416294cc4e78c8af0065cc7600ca728fed1d1d304fd9ff8a8c23471807c4b6e63d030eb8a23531946efa679b3c79339bbf83438e52c1f790f4745e'
